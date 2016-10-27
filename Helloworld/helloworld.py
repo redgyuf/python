8cafa6c446e5ab9e603c6e7a161dcef7a6821c08
Daniel Gal <faramirex070@gmail.com>
2016-10-27 15:44:53 +0200

    Add HelloWorld project

---
+++ Helloworld/helloworld.py
@@ -0,0 +1,17 @@
+import sys    #Import sys, to be able to read arguments
+
+#Converts all arguments into a single string.
+def GetArgumentsToString():
+    string = ""
+    for x in range(1, len(sys.argv)):
+        string += " " + sys.argv[x]
+    return string
+
+#Welcomes the given string, if empty string given, it welcomes the "World"
+def WelcomeUser(UserName):
+    if not UserName:   #Checks the given string for emptiness
+        UserName = " World"
+    print("Hello" + UserName + "!")
+
+#This is where the program runs, "WelcomeUser" function called.
+WelcomeUser(GetArgumentsToString())
